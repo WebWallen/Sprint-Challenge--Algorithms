8b9265e334bf1394b63755633f258089becb2bc4
Daniel Wallen <daniel.wallen1987@gmail.com>
2020-01-24 12:37:33 -0500

    Not sure first count_th solution would classify as recursive (which Lambda says not to use minus in rare cases anyway?) so I did another take.

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -11,10 +11,28 @@ def count_th(word):
     # Base case 2
     elif word == '':
         return 0
-    # Otherwise, run the recursive part
+    # Otherwise, run the count of th
     else: 
         return word.count('th')
 
 print(count_th('the'))
 print(count_th('thesis'))
 print(count_th('thethethe'))
+
+# Not sure above function counts as "recursion" (although it's a lot better imo) so here's a less efficient one.
+
+def less_efficient_count_th(word):
+    if len(word) <= 0:
+        return 0
+    if word[0] + word[1] == 'th':
+        print('Recursive tally: 1')
+        return 1
+    less_efficient_count_th(word[2:])
+    print('Recursive tally: 1') # Confirmed working because should only it on
+    return 1
+
+# This isn't perfect but calling it done as I'd use the first take in real life.
+
+print(less_efficient_count_th('the'))
+print(less_efficient_count_th('thesis'))
+print(less_efficient_count_th('thethethe'))
